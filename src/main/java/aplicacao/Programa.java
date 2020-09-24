5c3ba74ddf277b5de8ce165785553730f6ef144c
Alessandro Franca <alessandro0564@yahoo.com.br>
2020-09-24 18:27:49 -0300

    Exemplo Projeto JPA/Hibernate

---
+++ src/main/java/aplicacao/Programa.java
@@ -0,0 +1,36 @@
+package aplicacao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.hibernate.dialect.lock.PessimisticForceIncrementLockingStrategy;
+
+import dominio.Pessoa;
+
+public class Programa {
+
+	// Sistema com acesso ao banco de dados usando o JPA/Hibernate: FUNCIONANDO!!!
+	public static void main(String[] args) {
+		
+		// Nao esta aceitando o parametro null 
+		Pessoa pessoa1 = new Pessoa("Alessandro", "Ale@gmail.com");
+		Pessoa pessoa2 = new Pessoa("Darcilene", "darci@gmail.com");
+		Pessoa pessoa3= new Pessoa("Marcio", "mar@gmail.com");
+		
+		// Criando os objetos para ter acesso ao banco
+		EntityManagerFactory emf = 	Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		// Metodo para iniciar essa ooeração ao banco
+		em.getTransaction().begin();
+		// Salvando a pessoa no banco de dados
+		em.persist(pessoa1);
+		em.persist(pessoa2);
+		em.persist(pessoa3);
+		// Metodo para finalizar a operação
+		em.getTransaction().commit();
+		
+		System.out.println("Fim do programa");
+	}
+
+}
---
+++ src/main/java/dominio/Pessoa.java
@@ -0,0 +1,60 @@
+package dominio;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//@Getter #Nao esta funcionando o LOMBOK
+@Entity
+public class Pessoa implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String nome;
+	private String email;		
+	
+	public Pessoa(int id, String nome, String email) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+	}
+	
+	// Nao quer aceitar o parametro null: deixar sem parametro mesmo
+	public Pessoa(String nome, String email) {	
+		this.nome = nome;
+		this.email = email;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Pessoa [id=" + id + ", nome=" + nome + ", email=" + email + "]";
+	}		
+	
+	
+}
